f2bcaf86cb0544a9948aeaefc0b6405b3cd4c8ea
Ryan Gorup <gorup@users.noreply.github.com>
2020-03-20 16:52:26 -0700

    Change ebb-client to ebbflow

--- Formula/ebbflow.rb
+++ Formula/ebbflow.rb
@@ -1,7 +1,7 @@
 class Ebbflow < Formula
   desc "Client for using Ebbflow"
-  homepage "https://github.com/ebbflow-io/ebb-client"
-  url "https://github.com/ebbflow-io/ebb-client/releases/download/0.4.0/ebbflow-mac.zip"
+  homepage "https://github.com/ebbflow-io/ebbflow"
+  url "https://github.com/ebbflow-io/ebbflow/releases/download/0.4.0/ebbflow-mac.zip"
   sha256 "b177989abb5dcc615d8cdd429cec7526ef5f235eaac1621446847e663aadbb03"
   version "0.4.0"
 
