20a7772a3c1704fd703d9bdee35bfb419c04c3eb
Ryan Gorup <gorup@users.noreply.github.com>
2020-03-20 17:04:28 -0700

    Release 0.4.1

--- Formula/ebbflow.rb
+++ Formula/ebbflow.rb
@@ -1,9 +1,9 @@
 class Ebbflow < Formula
   desc "Client for using Ebbflow"
   homepage "https://github.com/ebbflow-io/ebbflow"
-  url "https://github.com/ebbflow-io/ebbflow/releases/download/0.4.0/ebbflow-mac.zip"
-  sha256 "b177989abb5dcc615d8cdd429cec7526ef5f235eaac1621446847e663aadbb03"
-  version "0.4.0"
+  url "https://github.com/ebbflow-io/ebbflow/releases/download/0.4.1/ebbflow-mac.zip"
+  sha256 "5605466bd7e7529d59a08b4fb061779a9cede38c5ca266e135a6bd2005a8989f"
+  version "0.4.1"
 
   def install
     bin.install "ebbflow"
